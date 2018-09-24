fa066e27b4429cbf565a924043b1c9a6299943f4
Licinius <florent.dellomo@gmail.com>
2018-09-24 12:04:29 +0200

    Feat : Ajout d'un evenement au calendrier
    - Ajout d'un évenement au calendrier de Coolendar
    - Ajout de la fonctionnalité sur le bouton cancel du fragment d'ajout d'event
    - Ajout d'un fragment pour la page d'accueil
    Bug : L'event semble être ajouté mais impossible de vérfier

--- mobile/src/main/java/com/tps/hasselhoff/coolendar/AddEventFragment.java
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/AddEventFragment.java
@@ -1,26 +1,38 @@
 package com.tps.hasselhoff.coolendar;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.TimeZone;
 
-public class AddEventFragment extends Fragment {
+public class AddEventFragment extends Fragment{
+
+    private Calendar date;
+    private EditText titleEditText;
+    private EditText descriptionEditText;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +42,14 @@ public class AddEventFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-        View rootView = inflater.inflate(R.layout.fragment_add_event,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_add_event,container,false);
+        titleEditText = rootView.findViewById(R.id.titleEvent);
+        descriptionEditText = rootView.findViewById(R.id.descriptionEvent);
         //Get heure courante
-        final Calendar date  = Calendar.getInstance();
+        date  = Calendar.getInstance();
         int hour = date.get(Calendar.HOUR_OF_DAY);
         int minute = date.get(Calendar.MINUTE);
-        SimpleDateFormat simpleTimeFormat  = new SimpleDateFormat("HH:mm");
+        final SimpleDateFormat simpleTimeFormat  = new SimpleDateFormat("HH:mm");
 
         final TextView myTime = rootView.findViewById(R.id.time);
         myTime.setText( simpleTimeFormat.format(date.getTime()));
@@ -44,7 +58,7 @@ public class AddEventFragment extends Fragment {
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 date.set(Calendar.HOUR,selectedHour);
                 date.set(Calendar.MINUTE,selectedMinute);
-                myTime.setText( selectedHour + ":" + selectedMinute);
+                myTime.setText(simpleTimeFormat.format(date.getTime()));
             }
         }, hour, minute, true);
 
@@ -73,6 +87,42 @@ public class AddEventFragment extends Fragment {
                 datePicker.show();
             }
         });
+        rootView.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               closeFragment();
+            }
+        });
+
+        rootView.findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveEvent();
+            }
+        });
         return rootView;
+    }
+
+    public void saveEvent(){
+        ContentResolver cr = getActivity().getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.CALENDAR_ID,1234);
+        values.put(CalendarContract.Events.DTSTART,date.getTimeInMillis());
+        values.put(CalendarContract.Events.DTEND,date.getTimeInMillis());
+        values.put(CalendarContract.Events.TITLE,titleEditText.getText().toString());
+
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().toString());
+        values.put(CalendarContract.Events.DESCRIPTION,descriptionEditText.getText().toString());
+
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values); // ZERO CHECK CAR LOL
+
+        long eventID = Long.parseLong(uri.getLastPathSegment());
+        Log.i("test",uri.toString());
+        closeFragment();
+    }
+
+    public void closeFragment(){
+        getFragmentManager().beginTransaction().remove(AddEventFragment.this).commit();
+        getActivity().findViewById(R.id.navigation_home).performClick();//Hack api <25.0.3
     }
 }
--- mobile/src/main/java/com/tps/hasselhoff/coolendar/MainActivity.java
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/MainActivity.java
@@ -3,15 +3,17 @@ package com.tps.hasselhoff.coolendar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private BottomNavigationView navigation;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -19,12 +21,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    showFragment(new HomeFragment());
                     return true;
                 case R.id.navigation_dashboard:
-                    FragmentTransaction transaction= getFragmentManager().beginTransaction();
-                    transaction.replace(R.id.fragment_container,new AddEventFragment());
-                    transaction.commit();
+                    showFragment(new AddEventFragment());
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -40,8 +40,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        showFragment(new HomeFragment());
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit();
     }
 
 }
