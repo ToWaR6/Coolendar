8e476596788bcb9fc4110fe5f94c5524f635be9b
Licinius <florent.dellomo@gmail.com>
2018-09-25 11:55:29 +0200

    Feat : Choix d'une date ou d'un type d'event pour la recherche
    - Une date peut être choisi pour la recherche
    - Un type d'evenement peut-être choisi pour la recherche
    - L'utilisateur peut cliquer sur une croix rouge pour supprimer la valeur

--- mobile/src/main/java/com/tps/hasselhoff/coolendar/HomeFragment.java
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/HomeFragment.java
@@ -1,22 +1,89 @@
 package com.tps.hasselhoff.coolendar;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class HomeFragment extends Fragment {
+import org.w3c.dom.Text;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    View rootView;
+    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE dd MMM yyyy");
+    Spinner spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ((AppCompatActivity) getActivity()).getSupportActionBar().show();
 
-        final View rootView = inflater.inflate(R.layout.home_fragment,container,false);
+        rootView = inflater.inflate(R.layout.home_fragment,container,false);
+        rootView.findViewById(R.id.deleteDate).setOnClickListener(this);
+        rootView.findViewById(R.id.deleteSpinner).setOnClickListener(this);
+        rootView.findViewById(R.id.searchButton).setOnClickListener(this);
+        final TextView myDate = rootView.findViewById(R.id.date);
+
+        Calendar date = Calendar.getInstance();
+        final DatePickerDialog datePicker = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar date = Calendar.getInstance();
+                date.set(year, month, dayOfMonth);
+                myDate.setText(simpleDateFormat.format(date.getTime()));
+
+            }
+        }, date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
+        myDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                datePicker.show();
+            }
+        });
+        spinner = rootView.findViewById(R.id.spinner);
+        spinner.setSelection(4);
         return rootView;
     }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.deleteDate:
+                ((TextView)rootView.findViewById(R.id.date)).setText(getContext().getResources().getString(R.string.default_date));
+                break;
+            case R.id.deleteSpinner:
+                spinner.setSelection(4,true);
+                break;
+            case R.id.searchButton:
+                searchEvents();
+                break;
+        }
+    }
+
+    private void searchEvents() {
+        String textDate = ((TextView) rootView.findViewById(R.id.date)).getText().toString();
+        Date date;
+        try {
+            date = simpleDateFormat.parse(textDate);
+        } catch (ParseException e) {
+            date = null;
+        }
+        if(date!=null){
+            Log.i("OK",simpleDateFormat.format(date));
+        }
+    }
+
 }
