476e7f0324704f62676fef47d2a44a355829b23d
Licinius <florent.dellomo@gmail.com>
2018-10-02 10:51:58 +0200

    Feat : Display notification sur montre et quand EVENT_REMINDER
    - Ajout d'un broadcast receiver sur les event_reminder
    - Ajout de l'affichage des notif. sur montre

--- mobile/src/main/java/com/tps/hasselhoff/coolendar/ReminderBroadcastReceiver.java
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/ReminderBroadcastReceiver.java
@@ -1,14 +1,40 @@
 package com.tps.hasselhoff.coolendar;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v4.app.NotificationCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 
 public class ReminderBroadcastReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent) {
-        android.util.Log.i("CalendarTest", "CalendarTest.onReceive called!");
+    public void onReceive(Context context, Intent intentReceived) {
+        showNotification(context);
+    }
+
+    private void showNotification(Context context) {
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(context,"default")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),R.mipmap.ic_launcher))
+                .setContentTitle("Notification!") // title for notification
+                .setContentText("Alarm Received")
+                .setSound(soundUri)// message for notification
+                .extend( new NotificationCompat.WearableExtender())//Display notification on wearable and Phone
+                .setAutoCancel(true); // clear notification after click
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
