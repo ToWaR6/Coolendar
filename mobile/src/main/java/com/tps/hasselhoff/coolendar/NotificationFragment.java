1c716cf864d0440753a4fd8b36b6186b26b8b7b3
Licinius <florent.dellomo@gmail.com>
2018-10-03 12:16:57 +0200

    Feat : Ajout des preferences
    Ajout des preferences et de l'ajout des preferences dans les shared Preferences à la selection des notifications

--- mobile/src/main/java/com/tps/hasselhoff/coolendar/NotificationFragment.java
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/NotificationFragment.java
@@ -3,9 +3,12 @@ package com.tps.hasselhoff.coolendar;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,35 +22,42 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NotificationFragment extends Fragment {
 
-    private static int cptNotif = 0;
+
     NotificationManager manager;
-    ArrayList<String> listTypeNotif;
     CheckBox birthday, sport, meeting, health, others;
+    SharedPreferences sharedPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        sharedPreferences = getContext().getSharedPreferences(Preferences.PREFS, Context.MODE_PRIVATE);
 
         final View rootView = inflater.inflate(R.layout.fragment_notification,container,false);
         this.manager = (NotificationManager) getContext().getSystemService(NOTIFICATION_SERVICE);
 
-        this.listTypeNotif = new ArrayList<>();
+
 
         this.birthday = rootView.findViewById(R.id.birthdayBox);
+        this.birthday.setHint(Preferences.PREF_BIRTHDAY);
         this.sport = rootView.findViewById(R.id.sportBox);
+        this.sport.setHint(Preferences.PREF_SPORT);
         this.meeting = rootView.findViewById(R.id.meetingBox);
+        this.meeting.setHint(Preferences.PREF_MEETING);
         this.health = rootView.findViewById(R.id.healthBox);
+        this.health.setHint(Preferences.PREF_HEALTH);
         this.others = rootView.findViewById(R.id.othersBox);
+        this.others.setHint(Preferences.PREF_OTHERS);
 
         CompoundButton.OnCheckedChangeListener onCheck = new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    listTypeNotif.add(buttonView.getText().toString());
+                    sharedPreferences.edit().putBoolean(buttonView.getHint().toString(),true).apply();
                 } else {
-                    listTypeNotif.remove(buttonView.getText().toString());
+
+                    sharedPreferences.edit().putBoolean(buttonView.getHint().toString(),false).apply();
                 }
-                createNotification("List:"+listTypeNotif.size(), listTypeNotif.toString());
+                createNotification("", "");
             }
         };
 
@@ -61,24 +71,8 @@ public class NotificationFragment extends Fragment {
     }
 
     private final void createNotification(String title, String text){
-
-        Intent intent = new Intent(getActivity(),MainActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 1, intent, 0);
-
-        Notification.Builder builder = new Notification.Builder(getActivity());
-
-        builder.setAutoCancel(true);
-        builder.setTicker("OMFG ! THERE'S A NOTIF !!!!!!");
-        builder.setContentTitle(title);
-        builder.setContentText(text);
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentIntent(pendingIntent);
-        builder.setOngoing(false);
-        builder.setSubText("WHAT A BEAUTIFUL NOTIFICATION");
-        builder.setNumber(100);
-        builder.setLocalOnly(false);
-        manager.notify(cptNotif++,  builder.build());
+        boolean b = true;
+        Log.i("TEST",""+sharedPreferences.getBoolean(Preferences.PREF_BIRTHDAY,b));
     }
 
 
---
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/Preferences.java
@@ -0,0 +1,14 @@
+package com.tps.hasselhoff.coolendar;
+
+
+public class Preferences {
+    public static final String PREFS = "PREFS_APP_COOLENDAR";
+    public static final String PREF_BIRTHDAY="Birthday";
+    public static final String PREF_MEETING="Meeting";
+    public static final String PREF_SPORT="Sport";
+    public static final String PREF_OTHERS="Others";
+    public static final String PREF_HEALTH = "Health";
+    private Preferences(){
+
+    }
+}
