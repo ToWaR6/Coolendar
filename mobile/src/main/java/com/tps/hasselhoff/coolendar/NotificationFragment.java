eade852b20b21fea539c1a50ef4c60f4d97d03b0
Licinius <florent.dellomo@gmail.com>
2018-10-04 12:14:08 +0200

    Feat : Notification checked
    - Lorsqu'une notification a été checké elle le reste prouvant que les preferences ont été enregistré.

--- mobile/src/main/java/com/tps/hasselhoff/coolendar/NotificationFragment.java
+++ mobile/src/main/java/com/tps/hasselhoff/coolendar/NotificationFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
@@ -28,26 +30,13 @@ public class NotificationFragment extends Fragment {
     SharedPreferences sharedPreferences;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
         sharedPreferences = getContext().getSharedPreferences(Preferences.PREFS, Context.MODE_PRIVATE);
-
         final View rootView = inflater.inflate(R.layout.fragment_notification,container,false);
         this.manager = (NotificationManager) getContext().getSystemService(NOTIFICATION_SERVICE);
 
-
-
-        this.birthday = rootView.findViewById(R.id.birthdayBox);
-        this.birthday.setHint(Preferences.PREF_BIRTHDAY);
-        this.sport = rootView.findViewById(R.id.sportBox);
-        this.sport.setHint(Preferences.PREF_SPORT);
-        this.meeting = rootView.findViewById(R.id.meetingBox);
-        this.meeting.setHint(Preferences.PREF_MEETING);
-        this.health = rootView.findViewById(R.id.healthBox);
-        this.health.setHint(Preferences.PREF_HEALTH);
-        this.others = rootView.findViewById(R.id.othersBox);
-        this.others.setHint(Preferences.PREF_OTHERS);
-
+        initCheckBoxes(rootView);
         CompoundButton.OnCheckedChangeListener onCheck = new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -57,7 +46,6 @@ public class NotificationFragment extends Fragment {
 
                     sharedPreferences.edit().putBoolean(buttonView.getHint().toString(),false).apply();
                 }
-                createNotification("", "");
             }
         };
 
@@ -70,10 +58,28 @@ public class NotificationFragment extends Fragment {
         return rootView;
     }
 
-    private final void createNotification(String title, String text){
-        boolean b = true;
-        Log.i("TEST",""+sharedPreferences.getBoolean(Preferences.PREF_BIRTHDAY,b));
+    private void initCheckBoxes(View rootView) {
+        ArrayList<CheckBox> checkBoxes = new ArrayList<>();
+        this.birthday = rootView.findViewById(R.id.birthdayBox);
+        this.birthday.setHint(Preferences.PREF_BIRTHDAY);
+        checkBoxes.add(this.birthday);
+        this.sport = rootView.findViewById(R.id.sportBox);
+        this.sport.setHint(Preferences.PREF_SPORT);
+        checkBoxes.add(this.sport);
+        this.meeting = rootView.findViewById(R.id.meetingBox);
+        this.meeting.setHint(Preferences.PREF_MEETING);
+        checkBoxes.add(this.meeting);
+        this.health = rootView.findViewById(R.id.healthBox);
+        this.health.setHint(Preferences.PREF_HEALTH);
+        checkBoxes.add(this.health);
+        this.others = rootView.findViewById(R.id.othersBox);
+        this.others.setHint(Preferences.PREF_OTHERS);
+        checkBoxes.add(this.others);
+        for(CheckBox c : checkBoxes) {
+            c.setChecked(sharedPreferences.getBoolean(c.getHint().toString(), false));
+        }
     }
+
 
 
 }
