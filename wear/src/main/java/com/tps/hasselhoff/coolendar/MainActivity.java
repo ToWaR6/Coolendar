fd23566f2ceb4e55455786af22ce076f9d98838a
Licinius <florent.dellomo@gmail.com>
2019-01-31 11:36:06 +0100

    Feat : Last commit

--- wear/src/main/java/com/tps/hasselhoff/coolendar/MainActivity.java
+++ wear/src/main/java/com/tps/hasselhoff/coolendar/MainActivity.java
@@ -1,12 +1,14 @@
 package com.tps.hasselhoff.coolendar;
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends WearableActivity {
@@ -21,14 +23,17 @@ public class MainActivity extends WearableActivity {
         if(title != null)
             ((EditText)findViewById(R.id.title)).setText(title.replace("[Sport]",""));
         setAmbientEnabled();
-
     }
 
     public void startSport(View button){
         String footing = getResources().getString(R.string.footing);
         String sportChosen = spinner.getSelectedItem().toString();
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if(sportChosen.equals(footing)){
-            showActivity(FootingActivity.class);
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                showActivity(FootingActivity.class);
+            else
+                Toast.makeText(this,getResources().getString(R.string.access_denied),Toast.LENGTH_SHORT).show();
         }
     }
 
